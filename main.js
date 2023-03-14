c26f2b6f65fef817262bff2e5ce4eeb1ad506c09
victorsworld <victordirect48@gmail.com>
2023-03-13 20:17:17 -0400

    finished

---
+++ main.js
@@ -0,0 +1,27 @@
+let par = document.querySelector('p');
+par.style.color = "lightblue";
+
+par.innerText = "Lorem ipsum dolor amet viral meh selfies drinking vinegar, intelligentsia poke flannel twee paleo enamel pin cray. Banjo celiac crucifix, kickstarter la croix air plant jianbing hashtag vinyl hell of man bun selvage schlitz banh mi. Tacos hella raclette quinoa blog, williamsburg adaptogen tbh. Hexagon af stumptown lumbersexual synth gentrify quinoa enamel pin celiac master cleanse. Truffaut typewriter shoreditch, semiotics iceland mixtape taxidermy umami distillery austin hashtag. Food truck synth wayfarers, street art banh mi actually authentic. Bitters tousled tattooed vegan neutra pug hell of fixie chia unicorn letterpress."
+
+let h1 = document.querySelector('h1')
+h1.style.fontSize = "48px"
+
+let class13 = document.querySelector('#item-13')
+class13.style.color = "rgba(0, 0, 0, 0.5)"
+class13.style.opacity = "0.5"
+
+let class3 = document.querySelector('#item-3')
+class3.innerText = "I say, Hi!"
+
+let myImage = document.querySelector("img");
+
+myImage.src = "https://f4.bcbits.com/img/a3190395404_16.jpg";
+myImage.style.height = "300px"
+
+let myImage1 = document.querySelector("#sunset");
+myImage1.style.height = "300px"
+
+let class16 = document.querySelector('#item-16')
+class16.innerText = "Won't get fooled again"
+
+
